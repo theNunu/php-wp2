d30da49bf66b1253c8fb6c3a0eaf1ee7666b0a64
Alexis Cepeda <alexkiller0408@hotmail.com>
2026-02-23 19:11:03 -0500

    2 commit

--- practica-man1.php
+++ practica-man1.php
@@ -4,4 +4,15 @@
  * Description: Plugin de prueba
  * Version: 1.0
  */
+require_once plugin_dir_path(__FILE__) . 'services/Saludar.php';
 
+add_action('wp_ajax_nopriv_dpti_saludar', 'dpti_saludar');
+add_action('wp_ajax_dpti_saludar', 'dpti_saludar');
+
+function dpti_saludar()
+{
+    $response = Saludar::mandarSaludo();
+
+    wp_send_json($response);
+
+}
